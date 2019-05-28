7f54d4b4c4ad65fac645c5c918afeb33071e03bc
qp214272757 <2846335879@qq.com>
2019-05-28 20:55:47 +0800

    999

---
+++ src/com/jiyun/test1.java
@@ -0,0 +1,24 @@
+package com.jiyun;
+
+public class test1 {
+    public static void main(String[] args) {
+        int a = 49;
+        int b = 46;
+        //加
+        int c = a + b;
+        //减
+        int d = a - b;
+        //乘
+        int e = a * b;
+        //除
+        int f = a / b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        for (int i = 1;i <= 100;i+=2){
+            System.out.println(i);
+        }
+    }
+}
